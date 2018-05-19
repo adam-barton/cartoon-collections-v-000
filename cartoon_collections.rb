93fb038a443c1a803efb84c60ecea225e8160de8
Adam Barton <adam.barton333@gmail.com>
2018-05-19 17:06:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,10 @@ def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
     cheeses.include?(cheese_types)
+    
     end
- 
+  if true 
+    cheeses
+  else
+    false 
 end
